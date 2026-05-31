a63c740b719fb4fabe671e3a5c9284009a80c2e3
vaishnavi-chaugule <chaugulevaishnavi38@gmail.com>
2026-05-31 22:11:34 +0530

    Add files via upload

---
+++ FindGreatestOfTwoNumbers.java
@@ -0,0 +1,22 @@
+package logicalpractice;
+import java.util.Scanner;
+
+public class FindGreatestOfTwoNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter fisrt number:");
+		int a = sc.nextInt();
+		
+		System.out.println("enter second number:");
+		int b = sc.nextInt();
+		
+		if(a>b) {
+			System.out.println("greatest number is:" + a);
+		}else {
+			System.out.println("greatest number is:" + b);
+		}
+
+	}
+
+}
